a977603234cbf71e2369537c38c53fdde9e854f9
katchenribaes <catarinaribaes@gmail.com>
2023-07-05 12:04:40 +0100

    fix docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,14 +14,11 @@ const config = {
   url: 'https://katchenribaes.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/onlineshop/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-
+  baseUrl: '/',
+  projectName: 'katchenribaes.github.io',
+  organizationName: 'onlineshop',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
